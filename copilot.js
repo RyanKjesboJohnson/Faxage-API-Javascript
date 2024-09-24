4739fb385cf9952d4b81418e7d13ffe88819e454
RyanKjesboJohnson <Ryanmark86@gmail.com>
2024-09-24 13:54:36 -0500

    Working version of faxage javascript for power automate

--- copilot.js
+++ copilot.js
@@ -1,8 +1,10 @@
 function ExecuteScript() { 
-    var table = document.getElementById("table1");
-    var rows = table.querySelectorAll("tr");
-    var currentRow;
-    for (let i=0; i< rows.length; i++){
+	var table = document.getElementById("table1");
+	console.log("table1:", table)
+	var rows = table.querySelectorAll("tr");
+    	console.log("rows:", rows);
+    	var currentRow;
+    	for (let i=0; i< rows.length; i++){
         const cells = rows[i].getElementsByTagName("td");
         for (let j=0; j<cells.length; j++){
             if (cells[j].innerText.includes("${FirstName} ${LastName}")){
@@ -10,59 +12,77 @@ function ExecuteScript() {
             }
         }
     };
+    console.log("currentRow:", currentRow);
     var cells = currentRow.getElementsByTagName("input");
+    console.log("cells:", cells);
+    console.log("checking the active field box")
     cells[0].checked = true;
-    if(${Program} === "CMLS") {
-        cells[2].value = "Central Minnesota Legal Services"}
-        else if (${Program} === "MDLC") {
-        cells[2].value = "Minnesota Disability Law Center"} 
-        else {cells[2].value = "Mid-Minnesota Legal Aid"}
-    let faxNumberCoverSheet;
-    switch (${faxNumber}) {
-        case 500418:
+    console.log("setting the program: ", "${Program}" )
+    var programFullName;
+    switch("${Program}") {
+    case "MMLA":
+    programFullName = "Mid-Minnesota Legal Aid";
+    break;
+    case "CMLS":
+    programFullName = "Central Minnesota Legal Services";
+    break;
+    case "MDLC":
+    programFullName = "Minnesota Disability Law Center";
+    break;
+    default:
+    console.log("no program match in switch");
+    }
+    console.log("this is the program full name:", programFullName);
+    cells[2].value = programFullName
+        console.log("setting the fax number with this code as reference:", "${FaxNumberCode}")    
+let faxNumberCoverSheet;
+    switch ("${FaxNumberCode}") {
+        case "500418":
             faxNumberCoverSheet = "(612)334-3402";
             break;
-        case 501682:
+        case "501682":
             faxNumberCoverSheet = "(320)253-9208";
             break;
-        case 412639:
+        case "412639":
             faxNumberCoverSheet = "(612)446-5773"
             break;
-        case 412637:
+        case "412637":
             faxNumberCoverSheet = "(612)446-5772";
             break;
-        case 409742 :
+        case "409742" :
             faxNumberCoverSheet = "(218)722-7885";
             break;
-        case 411531 :
+        case "411531" :
             faxNumberCoverSheet = "(507)322-1001";
             break;
-        case 500420 :
+        case "500420" :
             faxNumberCoverSheet = "(612)436-0499";
             break;
-        case 412641 :
+        case "412641" :
             faxNumberCoverSheet = "(612)446-5800";
             break;
-        case 410421 :
+        case "410421" :
             faxNumberCoverSheet = "(612)334-5755";
             break;
-        case 500419 :
+        case "500419" :
             faxNumberCoverSheet = "(612)332-4668";
             break;
-        case 410180 :
+        case "410180" :
             faxNumberCoverSheet = "(320)407-0410";
             break;
-        case 412435 :
+        case "412435" :
             faxNumberCoverSheet = "(320)253-5794";
             break;
-        case 410631 :
+        case "410631" :
             faxNumberCoverSheet = "(651 )222-0745";
             break;
         default:
             break;
     }
     cells[3].value = faxNumberCoverSheet;
+    console.log("checking the final three boxes")
     cells[4].checked = true;
     cells[5].checked = true;
     cells[7].checked = true;
-    }
+    return cells[6].name
+   }
